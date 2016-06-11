1c5cdea75380b17f501ee59270da6edd22b46779
precipitevolissimevolmente <necunoscut23@gmail.com>
2016-06-11 16:43:40 +0300

    start devel for LLAMA D

--- EmailService.php
+++ EmailService.php
@@ -2,7 +2,7 @@
 function sendEmailWithAttachment($file_name, $fileNameWithPath)
 {
 
-    $from_email = 'terrainfoplus.md@terrainfoplus.md'; //sender email
+    $from_email = 'llama@catatik.se'; //sender email
     $recipient_email = 'llama.results@gmail.com'; //recipient email
     $subject = 'LLAMA results for ' . $file_name; //subject of email
     $message = 'results were attached to this email'; //message body
